f6de6be13b2c935faf402f9393e63b4bea607bef
Fabricio :D <99661346+Fabriciofkt157@users.noreply.github.com>
2025-05-27 15:19:13 -0300

    Create binarios.c

---
+++ binarios.c
@@ -0,0 +1,107 @@
+
+#include <stdio.h>
+
+int main()
+{
+    int binario, decimal;
+    scanf("%d", &binario);
+    
+    switch(binario){
+        case 0:
+            printf("0\n");
+            break;
+        case 1:
+            printf("1\n");
+            break;
+        case 10:
+            printf("2\n");
+            break;
+        case 11:
+            printf("3\n");
+            break;
+        case 100:
+            printf("4\n");
+            break;
+        case 101:
+            printf("5\n");
+            break;
+        case 110:
+            printf("6\n");
+            break;
+        case 111:
+            printf("7\n");
+            break;
+        case 1000:
+            printf("8\n");
+            break;
+        case 1001:
+            printf("9\n");
+            break;
+        case 1010:
+            printf("10\n");
+            break;
+        case 1011:
+            printf("11\n");
+            break;
+        case 1100:
+            printf("12\n");
+            break;
+        case 1101:
+            printf("13\n");
+            break;
+        case 1110:
+            printf("14\n");
+            break;
+        case 1111:
+            printf("15\n");
+            break;
+        case 10000:
+            printf("16\n");
+            break;
+        case 10001:
+            printf("17\n");
+            break;
+        case 10010:
+            printf("18\n");
+            break;
+        case 10011:
+            printf("19\n");
+            break;
+        case 10100:
+            printf("20\n");
+            break;
+        case 10101:
+            printf("21\n");
+            break;
+        case 10110:
+            printf("22\n");
+            break;
+        case 10111:
+            printf("23\n");
+            break;
+        case 11000:
+            printf("24\n");
+            break;
+        case 11001:
+            printf("25\n");
+            break;
+        case 11010:
+            printf("26\n");
+            break;
+        case 11011:
+            printf("27\n");
+            break;
+        case 11100:
+            printf("28\n");
+            break;
+        case 11101:
+            printf("29\n");
+            break;
+        case 11110:
+            printf("30\n");
+            break;
+        default:
+            printf("31\n");
+    }
+    return 0;
+}
