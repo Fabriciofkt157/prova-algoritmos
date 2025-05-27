5cb538bca2ad1e8d97b662b27e223737dd8cfe16
Fabricio :D <99661346+Fabriciofkt157@users.noreply.github.com>
2025-05-27 15:31:17 -0300

    Update ano_bissexto.c

--- ano_bissexto.c
+++ ano_bissexto.c
@@ -6,12 +6,11 @@ int main()
     int ano;
     scanf("%d", &ano);
     if(ano % 400 == 0)
-        printf("Ano comum\n");
-    else {
-        if(ano % 4 == 0)
+        printf("Ano bissexto\n");
+    else if(ano % 4 == 0 && ano % 100 != 0)
             printf("Ano bissexto\n");
-        else
-            printf("Ano comum\n");
-    }
+    else
+        printf("Ano comum\n");
+    
     return 0;
 }
