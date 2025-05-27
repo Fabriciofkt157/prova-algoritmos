3fa399e2cc3978424534e4c3946caae39cfa09b5
Fabricio :D <99661346+Fabriciofkt157@users.noreply.github.com>
2025-05-27 13:50:47 -0300

    Create questao2.c

---
+++ questao2.c
@@ -0,0 +1,17 @@
+
+#include <stdio.h>
+
+int main()
+{
+    int a, ultimo;
+    scanf("%d", &a);
+    if(a <= 9 && a > -9) 
+        ultimo = a;
+    else 
+        ultimo = a % 10;
+    if(a < 0) 
+        ultimo = -ultimo;
+    printf("O ultimo digito de %d eh %d\n", a, ultimo);
+    
+    return 0;
+}
