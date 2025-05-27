f92520687a5c500caae5150553113fea3f2423c6
Fabricio :D <99661346+Fabriciofkt157@users.noreply.github.com>
2025-05-27 14:41:44 -0300

    Create temporizador.c

---
+++ temporizador.c
@@ -0,0 +1,16 @@
+
+#include <stdio.h>
+
+int main()
+{
+    int sI, sF, mF, graus, resto;
+    scanf("%d", &sI);
+    graus = sI / 3600;
+    resto = sI % 3600;
+    printf("%d\n", resto);
+    mF = resto / 60;
+    printf("%d\n", resto);
+    sF = resto % 60;
+    printf("%d grau(s), %d minuto(s) e %d segundo(s)\n", graus, mF, sF);
+    return 0;
+}
