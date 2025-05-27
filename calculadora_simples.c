9fa6da583c5b5ff83926797b62a1b81dc56d1c2d
Fabricio :D <99661346+Fabriciofkt157@users.noreply.github.com>
2025-05-27 15:57:41 -0300

    Create calculadora_simples.c

---
+++ calculadora_simples.c
@@ -0,0 +1,39 @@
+
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    int n1, n2, r;
+    char op;
+    scanf("%d %c %d", &n1, &op, &n2);
+    
+    switch(op){
+        case '+':
+            r = n1 + n2;
+            break;
+        case '-':
+            r = n1 - n2;
+            break;
+        case '*':
+            r = n1 * n2;
+            break;
+        case '/':
+            if(n2 == 0){
+                printf("resultado indefinido: divisor = 0");
+                return 0;
+            }
+            r = n1 / n2;
+        case '%':
+            r = n1 % n2;
+            break;
+        case '^':
+            r = pow(n1, n2);
+            break;
+        default:
+            printf("operador inválido");
+            return 0;
+    }
+    printf("%d %c %d = %d", n1, op, n2, r);
+    return 0;
+}
